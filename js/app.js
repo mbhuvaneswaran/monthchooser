7b01484b21540299622ca9e57ddf340b3016c02d
bhuvaneshwaranm <bhuvanesh12526@gmail.com>
2015-05-11 12:12:43 +0530

    working-1

--- js/app.js
+++ js/app.js
@@ -6,14 +6,41 @@ app.directive("monthChooser",function(){
     var directive={
 
         link:function(scope,elm,attr){
-            debugger;
+            scope.yearMonths=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+
+            //elm[0].addEventListener("click",onclickEvent);
+            elm[0].querySelector("input").addEventListener("focus",getOnFocusEvent);
+            elm[0].querySelector(".datepicker-months").addEventListener("blur",getBlurEvent(scope));
+            elm[0].ownerDocument.body.addEventListener("click",getClickEvent(scope));
+             scope.pickerHide=true;
+             function onclickEvent(){
+                     event.srcElement.style.display=""
+                 }
+                 function getOnFocusEvent(){
+
+                     scope.$apply(scope.pickerHide=false);
+
+                 }
+                 function getBlurEvent(scope){
+                     return function(){
+                         scope.pickerHide=true;
+                     }
+                 }
+                 function getClickEvent(scope){
+                     return function(){
+
+                     }
+                 }
         },
         scope:{
             monthModel:'='
+
         },
         restrict:"EA",
         templateUrl:"partials/monthpicker.html"
 
     }
     return directive;
+
 })
